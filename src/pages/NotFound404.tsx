58b4b5185f29cb83bf42739c22de01c6f0dd22b6
Jhontri Boyke <51961172+jhontriboyke@users.noreply.github.com>
2024-03-15 00:10:21 +0700

    Update 404 Page

--- src/pages/NotFound404.tsx
+++ src/pages/NotFound404.tsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom';
 
 const NotFound404 = () => {
     return (
-        <div className="flex-[1] grid place-items-center ">
+        <div className="flex-[1] grid place-items-center py-20 px-5">
             <div className="flex flex-col gap-10 text-center">
-                <h1 className="text-5xl tracking-wide font-light text-primary-black">
+                <h1 className="text-5xl tracking-wide font-light text-primary-black/40">
                     404 Page Not Found
                 </h1>
+                <h2 className="text-3xl">Page is still under construction</h2>
                 <Link to={'/'} className="underline">
                     Back to Home
                 </Link>
