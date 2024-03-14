236c7d1ace400333e3e70d7ede74636f7cb8606b
Jhontri Boyke <51961172+jhontriboyke@users.noreply.github.com>
2024-03-14 18:09:31 +0700

    Define page routes
    - Page routes using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import About from './pages/About';
 
 /*
 TODO
@@ -13,7 +15,14 @@ const App = () => {
     return (
         <>
             <Navbar />
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Home />} />
+                <Route path="/learn" element={<Home />} />
+                <Route path="/news" element={<Home />} />
+                <Route path="/donate" element={<Home />} />
+            </Routes>
             <Footer />
         </>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { MenuIcon } from 'lucide-react';
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -12,13 +13,14 @@ const Navbar = () => {
                     <img src="\photos\logo.png" alt="" />
                 </div>
                 <div className="hidden lg:flex flex-row items-center font-playfair gap-8 text-white">
-                    <span>Home</span>
-                    <span>About Us</span>
-                    <span>Projects</span>
-                    <span>Learn</span>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/about'}>About Us</Link>
+                    <Link to={'/project'}>Projects</Link>
+                    <Link to={'/learn'}>Learn</Link>
+                    <Link to={'/news'}>News</Link>
                     <span>News</span>
                     <Button className="bg-primary-red hover:bg-primary-red-darker p-2">
-                        Donate Now
+                        <Link to={'/donate'}>Donate Now</Link>
                     </Button>
                 </div>
             </div>
